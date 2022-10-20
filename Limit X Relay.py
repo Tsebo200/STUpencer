5a75bfd8a4827f9ae4780e8b87b74e97664a1211
Tsebo Rams <200200@virtualwindow.co.za>
2022-10-20 11:29:38 +0200

    Limit x Relay Created
    These limit switches can switch on the Rely now

---
+++ Limit X Relay.py
@@ -0,0 +1,36 @@
+import time
+import board
+import digitalio
+from digitalio import DigitalInOut, Direction, Pull
+
+limitOne = digitalio.DigitalInOut(board.GP15)
+limitOne.direction = digitalio.Direction.INPUT
+limitOne.pull = digitalio.Pull.UP
+
+limitTwo = digitalio.DigitalInOut(board.GP16)
+limitTwo.direction = digitalio.Direction.INPUT
+limitTwo.pull = digitalio.Pull.UP
+
+relay = DigitalInOut(board.GP17)
+relay.direction = Direction.OUTPUT
+
+# switch = digitalio.DigitalInOut(board.SLIDE_SWITCH)
+# switch.direction = digitalio.Direction.INPUT
+# switch.pull = digitalio.Pull.UP
+
+while True:
+    # print("GP15",limitOne.value)
+    # print("GP16",limitTwo.value)
+    if limitOne.value:
+        print("GP15")
+        relay.value = True
+    else:
+        relay.value = False
+
+    if limitTwo.value:
+        print("GP16")
+    # elif switch.value:
+    #     print((-1,))
+    # else:
+    #     print((0,))
+    time.sleep(0.1)
