3c07e9ae0f34340eb70efcdb4d020340509712ec
Tsebo Rams <200200@virtualwindow.co.za>
2022-10-20 08:03:49 +0200

    Testing The Limit Switch
    Mixed Python and circuit Python syntax

--- code.py
+++ code.py
@@ -1,27 +1,46 @@
-from multiprocessing.sharedctypes import Value
 import board
 import digitalio
 import time
 relay = digitalio.DigitalInOut(board.GP16)
-
 from digitalio import DigitalInOut, Direction, Pull #Float Switch
+
+relay = Pin(GP16, Pin.Out)
+btn = Pin(GP17, Pin.In)
 
 btn = DigitalInOut(board.SWITCH)
 btn.direction = Direction.INPUT
 btn.pull = Pull.UP
 
+
 while True:
+    if not btn.value:
+        print("BTN is down")
+    else:
+        print("BTN is up")
+
+
+time.sleep(0.1) 
+
+
+# Normal Python code
+# while True:
+#     logic_state = button.value()
+#     if logic_state == True:
+#         relay.on()
+#     else:
+#         relay.off()
+
+
+
 
     # Add If statement
-    relay.value = True
-    time.sleep(0.5)
+    # relay.value = True
+    # time.sleep(0.5)
 
-    relay.value = False
-    time.sleep(0.5)
+    # relay.value = False
+    # time.sleep(0.5)
 
 
-
-    
 
 
 
